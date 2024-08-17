d7d4e899e52e92fb6c9a4eb43ab27442a04af77a
st1lk3r <St1lk3r@bk.ru>
2024-08-17 16:28:02 +0300

    Added conection to DB

--- server/services/posts-uservice/src/hello.cpp
+++ server/services/posts-uservice/src/hello.cpp
@@ -1,6 +1,7 @@
 #include "hello.hpp"
 
 #include <fmt/format.h>
+#include <userver/components/component_context.hpp>
 
 #include <userver/server/handlers/http_handler_base.hpp>
 
@@ -18,13 +19,22 @@ class Hello final : public userver::server::handlers::HttpHandlerBase {
 public:
   static constexpr std::string_view kName = "handler-hello";
 
+  Hello(const userver::components::ComponentConfig& config,
+           const userver::components::ComponentContext& context) : HttpHandlerBase(config,context),
+            pg_cluster_(context.FindComponent<userver::components::Postgres>("postgres-db-1").GetCluster()) {
+
+  }
+
   using HttpHandlerBase::HttpHandlerBase;
 
   std::string HandleRequestThrow(
       const userver::server::http::HttpRequest &request,
       userver::server::request::RequestContext &) const override {
+
     return service_template::SayHelloTo(request.GetArg("name"));
   }
+private:
+  userver::storages::postgres::ClusterPtr pg_cluster_;
 };
 
 } // namespace
--- server/services/posts-uservice/src/main.cpp
+++ server/services/posts-uservice/src/main.cpp
@@ -3,6 +3,7 @@
 #include <userver/clients/http/component.hpp>
 #include <userver/server/handlers/ping.hpp>
 #include <userver/server/handlers/tests_control.hpp>
+#include <userver/storages/postgres/component.hpp>
 #include <userver/testsuite/testsuite_support.hpp>
 #include <userver/utils/daemon_run.hpp>
 
@@ -13,6 +14,7 @@ int main(int argc, char* argv[]) {
                             .Append<userver::server::handlers::Ping>()
                             .Append<userver::components::TestsuiteSupport>()
                             .Append<userver::components::HttpClient>()
+                            .Append<userver::components::Postgres>("postgres-db-1")
                             .Append<userver::clients::dns::Component>()
                             .Append<userver::server::handlers::TestsControl>();
 
