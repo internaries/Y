62262eab76d14049a2d856fc086fd149fc2fed2d
st1lk3r <St1lk3r@bk.ru>
2024-08-18 18:32:45 +0300

    Added mock data

---
+++ server/databases/core-pgsql/data/mockData.sql
@@ -0,0 +1,5 @@
+INSERT INTO posts(id,description,media_url,author_id,author_name,author_avatar_url,created_at)
+VALUES ('749e5dc8-4c3b-4158-95c1-dc2ff9aab17b','Hehe silly', ('y-media', 'cat_cute_blabla.jpg'),'46775165-7542-49d9-a3b2-a7878980a2bc','kittycat', ('y-media', 'cat_cute_blabla.jpg'),'2024-08-17T15:50:20.711Z');
+
+INSERT INTO posts(id,description,media_url,author_id,author_name,author_avatar_url,created_at)
+VALUES ('dd1870ab-fb0d-4b79-a890-ce62a091b77a','Just settin up my YYYYYY','','6cca3ced-9615-4e2c-bae4-580c86d78710','cool','','2024-08-17T15:29:16.307Z');
