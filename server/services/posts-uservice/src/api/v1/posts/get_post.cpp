415729ab8b5a92a3c3084746434ff062e92a366d
st1lk3r <St1lk3r@bk.ru>
2024-08-18 13:54:23 +0300

    Added recieving post from id

--- server/services/posts-uservice/src/api/v1/posts/get_post.cpp
+++ server/services/posts-uservice/src/api/v1/posts/get_post.cpp
@@ -1,5 +1,6 @@
 #include "get_post.hpp"
 
+#include <boost/lexical_cast.hpp>
 #include <fmt/format.h>
 #include <userver/components/component_context.hpp>
 
@@ -10,11 +11,17 @@
 #include <userver/storages/postgres/cluster.hpp>
 #include <userver/storages/postgres/component.hpp>
 
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_io.hpp>
+
 
 namespace posts_uservice {
+std::string ToLink(const S3_url &url) {
+  return "https://storage.yandexcloud.net/" + url.bucket + "/" + url.key;
+
+}
 
 namespace {
-
 class GetPost final : public userver::server::handlers::HttpHandlerBase {
 public:
   static constexpr std::string_view kName = "handler-get-post";
@@ -30,7 +37,43 @@ public:
   std::string HandleRequestThrow(
       const userver::server::http::HttpRequest &request,
       userver::server::request::RequestContext &) const override {
-    return "I getted post";
+    auto post_id_as_text = request.GetPathArg("postId");
+    boost::uuids::uuid post_id;
+    try {
+      post_id = boost::lexical_cast<boost::uuids::uuid>(post_id_as_text);
+    }
+    catch (std::bad_cast) {
+      auto& response = request.GetHttpResponse();
+      response.SetStatus(userver::server::http::HttpStatus::kBadRequest);
+      return "Bad Id";
+    }
+
+    auto res = pg_cluster_->Execute(userver::storages::postgres::ClusterHostType::kMaster,"SELECT id ,description, media_url, author_id, author_name, author_avatar_url, created_at from posts WHERE id = $1",post_id);
+    if(res.IsEmpty()) {
+      auto& response = request.GetHttpResponse();
+      response.SetStatus(userver::server::http::HttpStatus::kNotFound);
+      return "Id does not exist";
+    }
+    auto parsed = res.AsSingleRow<RowResponse>(userver::storages::postgres::kRowTag);
+
+    userver::formats::json::ValueBuilder builder;
+
+    builder["id"] = post_id_as_text;
+    builder["text"] = parsed.description.value();
+    builder["mediaUrl"] = (parsed.media_url.has_value() ? ToLink(parsed.media_url.value()) : "");
+    builder["authorId"] = (parsed.author_id.has_value() ? boost::uuids::to_string(parsed.author_id.value()) : "");
+    builder["authorName"] = parsed.author_name;
+    builder["authorAvatarUrl"] = (parsed.author_avatar_url.has_value() ? ToLink(parsed.author_avatar_url.value()) : "");
+    builder["createdAt"] = parsed.created_at;
+
+
+
+    auto& response = request.GetHttpResponse();
+    response.SetStatus(userver::server::http::HttpStatus::kOk);
+    return ToPrettyString(builder.ExtractValue());
+
+
+
   }
 private:
   userver::storages::postgres::ClusterPtr pg_cluster_;
