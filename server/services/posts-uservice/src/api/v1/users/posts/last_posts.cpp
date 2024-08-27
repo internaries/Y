6f8d7bdc20a8f1bb27615634c6e9f10c0c3aea8c
Dmitry Deruzhinsky <merlinusthewizard@gmail.com>
2024-08-27 13:26:02 +0300

    Moved pagination arguments parsing to utils::fields to use them in the feed pagination

--- server/services/posts-uservice/src/api/v1/users/posts/last_posts.cpp
+++ server/services/posts-uservice/src/api/v1/users/posts/last_posts.cpp
@@ -25,38 +25,6 @@
 
 namespace posts_uservice {
 
-int ParseSizeArgument(const std::string& size_argument) {
-  if (size_argument.empty()) {
-    return POSTS_USERVICE_LAST_POSTS_DEFAULT_PAGE_SIZE;
-  }
-
-  std::size_t pos = 0;
-  int result;
-  try {
-    result = std::stoi(size_argument, &pos);
-    if (pos != size_argument.size()) {
-      throw std::invalid_argument("");
-    }
-    if (result < 1 || result > 100) {
-      throw std::out_of_range("");
-    }
-  } catch (const std::out_of_range&) {
-    throw errors::ValidationException("size", "Size of page is limited in range from 1 to 100");
-  } catch (const std::invalid_argument&) {
-    throw errors::ValidationException("size", "Int is expected");
-  }
-  return result;
-}
-
-std::chrono::system_clock::time_point ParsePageArgument(const std::string& page_argument) {
-  try {
-    return userver::utils::datetime::Stringtime(page_argument, userver::utils::datetime::kDefaultTimezone,
-                                                userver::utils::datetime::kRfc3339Format);
-  } catch (const userver::utils::datetime::DateParseError&) {
-    throw errors::ValidationException("page", "Page token is not valid");
-  }
-}
-
 namespace {
 
 class LastPosts final : public userver::server::handlers::HttpHandlerBase {
@@ -71,7 +39,7 @@ class LastPosts final : public userver::server::handlers::HttpHandlerBase {
                                   user_id, size);
 
     } else {
-      std::chrono::system_clock::time_point page = ParsePageArgument(page_argument);
+      std::chrono::system_clock::time_point page = utils::ParsePaginationArgument(page_argument);
       return pg_cluster_->Execute(userver::storages::postgres::ClusterHostType::kMaster,
                                   fmt::format("SELECT {} from posts WHERE author_id = $1 "
                                               "AND created_at < $3 "
@@ -99,7 +67,7 @@ class LastPosts final : public userver::server::handlers::HttpHandlerBase {
     const std::string& page_argument = request.GetArg("page");
 
     const std::string& size_argument = request.GetArg("size");
-    int size = ParseSizeArgument(size_argument);
+    int size = utils::ParsePaginationSizeArgument(size_argument);
 
     auto res = GetPostsFromDb(page_argument, user_id, size);
 
--- server/services/posts-uservice/src/api/v1/users/posts/last_posts.hpp
+++ server/services/posts-uservice/src/api/v1/users/posts/last_posts.hpp
@@ -9,10 +9,7 @@
 
 namespace posts_uservice {
 
-#define POSTS_USERVICE_LAST_POSTS_DEFAULT_PAGE_SIZE 10
 
-int ParseSizeArgument(const std::string& size_argument);
-std::chrono::system_clock::time_point ParsePageArgument(const std::string& page_argument);
 
 void AppendLastPosts(userver::components::ComponentList& component_list);
 
