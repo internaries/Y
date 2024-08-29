ccbb961aab1d68919bfcb81e6aa55e569e029f0e
tiimuur <timkawert.ru@gmail.com>
2024-08-29 18:09:17 +0300

    tests edited

--- server/services/posts-uservice/tests/test_get_post.py
+++ server/services/posts-uservice/tests/test_get_post.py
@@ -26,7 +26,7 @@ async def test_goodData(service_client):
 
     expected_data = {
         "id": "749e5dc8-4c3b-4158-95c1-dc2ff9aab17b",
-        "text": "Hehe silly",
+        "description": "Hehe silly",
         "mediaUrl": "https://storage.yandexcloud.net/y-media/cat_cute_blabla.jpg",
         "authorId": "46775165-7542-49d9-a3b2-a7878980a2bc",
         "authorName": "kittycat",
@@ -41,7 +41,7 @@ async def test_goodData(service_client):
 
     expected_data = {
         "id": "dd1870ab-fb0d-4b79-a890-ce62a091b77a",
-        "text": "Just settin up my YYYYYY",
+        "description": "Just settin up my YYYYYY",
         "mediaUrl": "",
         "authorId": "6cca3ced-9615-4e2c-bae4-580c86d78710",
         "authorName": "cool",
