69958cc49cf948022850eec18a3285c659684375
tiimuur <timkawert.ru@gmail.com>
2024-08-31 16:57:15 +0300

    subscribes handle ready

--- server/services/posts-uservice/src/api/v1/users/subscribers/subscribers.cpp
+++ server/services/posts-uservice/src/api/v1/users/subscribers/subscribers.cpp
@@ -1,17 +1,22 @@
 #include "subscribers.hpp"
 
 #include <fmt/format.h>
+#include <boost/uuid/uuid.hpp>
 #include <userver/components/component_context.hpp>
 
 #include <userver/formats/serialize/common_containers.hpp>
 #include <userver/server/handlers/http_handler_base.hpp>
 
-#include "models/pagination.hpp"
-#include "models/post.hpp"
+#include "models/user.hpp"
+#include "utils/errors.hpp"
+#include "utils/fields.hpp"
 #include "userver/storages/secdist/exceptions.hpp"
 
 #include <userver/storages/postgres/cluster.hpp>
+#include <userver/storages/postgres/cluster_types.hpp>
 #include <userver/storages/postgres/component.hpp>
+#include <userver/storages/postgres/io/row_types.hpp>
+#include <userver/storages/postgres/postgres_fwd.hpp>
 
 namespace posts_uservice {
 
@@ -29,11 +34,31 @@ class GetSubscribers final : public userver::server::handlers::HttpHandlerBase {
 
   std::string HandleRequestThrow(const userver::server::http::HttpRequest& request,
                                  userver::server::request::RequestContext&) const override {
-    // can be useful for hide subscribers for someone
+    // can be useful for hide subscriptions for someone
     const std::string& user_authorized_id_argument = request.GetHeader("System-Design-User-Id");
     const std::string& user_id_argument = request.GetPathArg("userId");
 
-    return "anything";
+    const auto user_authorized_id = utils::ParseUUIDArgument(user_id_argument);
+    const auto user_id = utils::ParseUUIDArgument(user_id_argument);
+
+    auto user_exists = pg_cluster_->Execute(
+      userver::storages::postgres::ClusterHostType::kMaster,
+      "SELECT name FROM users WHERE id = $1", user_id);
+
+    if(user_exists.IsEmpty()) {
+      throw errors::NotFoundException("user", "User with this id not found");
+    }
+
+    auto res = pg_cluster_->Execute(
+      userver::storages::postgres::ClusterHostType::kMaster,
+      "SELECT folower_id FROM follows WHERE folowee_id = $1", user_id);
+    
+    auto users = res.AsSetOf<models::UserResponse>(userver::storages::postgres::kRowTag);
+
+    userver::formats::json::ValueBuilder response;
+    response["users"] = users;
+    
+    return userver::formats::json::ToPrettyString(response.ExtractValue());
   }
 
  private:
