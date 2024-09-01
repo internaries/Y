dd257fdd84528e25f2af8bfb26c3894f6e52643e
st1lk3r <St1lk3r@bk.ru>
2024-09-01 18:40:32 +0300

    Added mock data for tests

---
+++ server/databases/core-pgsql/data/testCreateSubsrcibe.sql
@@ -0,0 +1,4 @@
+INSERT INTO users(id,name,login,password) VALUES('6cca3ced-9615-4e2c-bae4-580c86d78710','Lox','Login','Password');
+INSERT INTO users(id,name,login,password) VALUES('46775165-7542-49d9-a3b2-a7878980a2bc','Lox123','Login123','Password123');
+
+INSERT INTO follows(folowee_id,folower_id) VALUES('6cca3ced-9615-4e2c-bae4-580c86d78710','46775165-7542-49d9-a3b2-a7878980a2bc');
